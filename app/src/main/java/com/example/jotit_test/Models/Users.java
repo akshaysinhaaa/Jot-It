f2c16b83c7250b71a49c103147eeee5da82cbef8
ANAY <anaypatil034@gmail.com>
2023-03-02 16:15:59 +0530

    Firebase Sign Up done. Database not Working

---
+++ app/src/main/java/com/example/jotit_test/Models/Users.java
@@ -0,0 +1,46 @@
+package com.example.jotit_test.Models;
+
+public class Users {
+    String userName , mail , password, userid;
+
+    public Users(String userName, String mail, String password, String userid) {
+        this.userName = userName;
+        this.mail = mail;
+        this.password = password;
+    }
+
+    public Users(){}
+
+    //Signup constructor
+    public Users(String userName, String mail, String password)
+    {
+        this.userName = userName;
+        this.mail = mail;
+        this.password = password;
+
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
--- app/src/main/java/com/example/jotit_test/SignUpActivity.java
+++ app/src/main/java/com/example/jotit_test/SignUpActivity.java
@@ -1,25 +1,65 @@
 package com.example.jotit_test;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Toast;
 
+import com.example.jotit_test.Models.Users;
 import com.example.jotit_test.databinding.ActivitySignUpBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class SignUpActivity extends AppCompatActivity {
 
     ActivitySignUpBinding binding;
-
+    private FirebaseAuth auth;
+    FirebaseDatabase database;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
         binding = ActivitySignUpBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        auth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+
+        binding.btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                auth.createUserWithEmailAndPassword(binding.etEmail.getText().toString(), binding.etPassword.getText().toString()).
+                        addOnCompleteListener(new OnCompleteListener<AuthResult>()
+                {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if(task.isSuccessful())
+                        {
+                            Users user = new Users(binding.etUserName.getText().toString() , binding.etEmail.getText().toString() , binding.etPassword.getText().toString());
+
+                            String id = task.getResult().getUser().getUid();
+                            database.getReference().child("Users").child(id).setValue(user);
+
+                            Toast.makeText(SignUpActivity.this, "User Created Successfully", Toast.LENGTH_SHORT).show();
+                        }
+                        else
+                        {
+                            Toast.makeText(SignUpActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+            }
+        });
     }
 }
