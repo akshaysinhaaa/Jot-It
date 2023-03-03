08b5250312fbce275a335954b0072c6cb36336bd
Akshay Sinha <alt.akshay.sinha@gmail.com>
2023-03-03 12:25:19 +0530

    removed app bar issue and changed the Txt view to Click for sign up

--- app/src/main/java/com/example/jotit_test/SignInActivity.java
+++ app/src/main/java/com/example/jotit_test/SignInActivity.java
@@ -7,6 +7,7 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Toast;
 
 import com.example.jotit_test.databinding.ActivitySignInBinding;
@@ -24,6 +25,9 @@ public class SignInActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // To hide the App Bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide(); //Hides Action Bar
         binding = ActivitySignInBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
