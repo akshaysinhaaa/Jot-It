d444ee627e68591e200e4d9a0ed6626fd1c52d30
hazemayn <bansalrohan77@gmail.com>
2023-04-19 12:42:02 +0530

    signIn Done and menu bar

--- app/src/main/java/com/example/jotit_test/MainActivity.java
+++ app/src/main/java/com/example/jotit_test/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.jotit_test;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -17,16 +21,76 @@ import android.widget.Toast;
 
 import com.example.jotit_test.databinding.ActivityMainBinding;
 import com.google.firebase.auth.FirebaseAuth;
+import androidx.core.content.ContextCompat;
+import android.content.pm.PackageManager;
+import androidx.core.app.ActivityCompat;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.Continuation;
+import android.provider.MediaStore;
+import android.database.Cursor;
+import android.provider.OpenableColumns;
+import com.google.android.gms.tasks.OnCompleteListener;
+import java.util.*;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final int PERMISSION_REQUEST_CODE = 200;
+    private static final int REQUEST_CODE_PICK_FILE = 1;
+    private Uri selectedFileUri;
     ActivityMainBinding binding;
     FirebaseAuth auth;
+    private FirebaseStorage storage = FirebaseStorage.getInstance();
+    private StorageReference storageRef = storage.getReference();
 
     Button Maths, Physics, Coding, Online, DLD;
     Button logout_button;
+    Button upload_button;
 
+    private void launchFileChooser() {
+        Log.d(TAG,"running log filechooser");
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("/");
+        startActivityForResult(intent, REQUEST_CODE_PICK_FILE);
 
+    }
+    private void uploadFile(Uri fileUri) {
+        // Get the file name from the file URI
+        String fileName = getFileName(fileUri);
+
+        // Create a storage reference to the file location
+        StorageReference fileRef = storageRef.child("uploads/" + fileName);
+
+        // Upload the file to Firebase Storage
+        UploadTask uploadTask = fileRef.putFile(fileUri);
+
+        // Get the download URL of the uploaded file and handle the upload result
+        uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+            @Override
+            public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                if (!task.isSuccessful()) {
+                    throw Objects.requireNonNull(task.getException());
+                }
+
+                return fileRef.getDownloadUrl();
+            }
+        }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+            @Override
+            public void onComplete(@NonNull Task<Uri> task) {
+                if (task.isSuccessful()) {
+                    // Get the download URL of the uploaded file
+                    Uri downloadUri = task.getResult();
+
+                    // Handle successful upload
+                    Toast.makeText(MainActivity.this, "File uploaded successfully", Toast.LENGTH_SHORT).show();
+                } else {
+                    // Handle unsuccessful upload
+                    Toast.makeText(MainActivity.this, "Error uploading file: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,6 +110,21 @@ public class MainActivity extends AppCompatActivity {
 
         auth = FirebaseAuth.getInstance();
         logout_button = findViewById(R.id.logout_button);
+        upload_button = findViewById(R.id.upload_button);
+
+        upload_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSION_REQUEST_CODE);
+                    Log.d(TAG,"vrnonclickif");
+                }
+                else {
+                    launchFileChooser();
+                }
+            }
+
+        });
 
         logout_button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -103,14 +182,52 @@ public class MainActivity extends AppCompatActivity {
         Uri uri = Uri.parse(s);
         startActivity(new Intent(Intent.ACTION_VIEW, uri));
     }
-
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu , menu);
-        return super.onCreateOptionsMenu(menu);
-    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
+        if (requestCode == REQUEST_CODE_PICK_FILE && resultCode == RESULT_OK) {
+            selectedFileUri = data.getData();
+            uploadFile(selectedFileUri);
+        }
+    }
+    @SuppressLint("Range")
+    private String getFileName(Uri uri) {
+        String result = null;
+        if (uri.getScheme().equals("content")) {
+            Cursor cursor = getContentResolver().query(uri, null, null, null, null);
+            try {
+                if (cursor != null && cursor.moveToFirst()) {
+                    result = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME));
+                }
+            } finally {
+                cursor.close();
+            }
+        }
+        if (result == null) {
+            result = uri.getPath();
+            int cut = result.lastIndexOf('/');
+            if (cut != -1) {
+                result = result.substring(cut + 1);
+            }
+        }
+        return result;
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == PERMISSION_REQUEST_CODE && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            launchFileChooser();
+        }
+    }
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        MenuInflater inflater = getMenuInflater();
+//        inflater.inflate(R.menu.menu , menu);
+//        return super.onCreateOptionsMenu(menu);
+//    }
+//
 //    @Override
 //    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
 //
