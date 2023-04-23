7a550298c659d6b0f4a7eef517df87d1d0bda4ac
anay <anaypatil034@gmail.com>
2023-04-24 03:03:50 +0600

    xyz

---
+++ app/src/main/java/com/example/jotit_test/UserProfile.java
@@ -0,0 +1,80 @@
+package com.example.jotit_test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+class editProfileActivity extends AppCompatActivity {
+    EditText editName, editEmail, editUsername, editPassword;
+    Button saveButton;
+    String nameUser, emailUser, usernameUser, passwordUser;
+    DatabaseReference reference;
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile);
+        reference = FirebaseDatabase.getInstance().getReference("users");
+        editName = findViewById(R.id.etUserName);
+        editEmail = findViewById(R.id.etEmail);
+//      editUsername = findViewById(R.id.editUsername);
+        editPassword = findViewById(R.id.etPassword);
+        saveButton = findViewById(R.id.saveButton);
+        showData();
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (isNameChanged() || isPasswordChanged() || isEmailChanged()){
+                    Toast.makeText(editProfileActivity.this, "Saved", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(editProfileActivity.this, "No Changes Found", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+    private boolean isNameChanged() {
+        if (!nameUser.equals(editName.getText().toString())){
+            reference.child(usernameUser).child("name").setValue(editName.getText().toString());
+            nameUser = editName.getText().toString();
+            return true;
+        } else {
+            return false;
+        }
+    }
+    private boolean isEmailChanged() {
+        if (!emailUser.equals(editEmail.getText().toString())){
+            reference.child(usernameUser).child("email").setValue(editEmail.getText().toString());
+            emailUser = editEmail.getText().toString();
+            return true;
+        } else {
+            return false;
+        }
+    }
+    private boolean isPasswordChanged() {
+        if (!passwordUser.equals(editPassword.getText().toString())){
+            reference.child(usernameUser).child("password").setValue(editPassword.getText().toString());
+            passwordUser = editPassword.getText().toString();
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public void showData(){
+        Intent intent = getIntent();
+        nameUser = intent.getStringExtra("name");
+        emailUser = intent.getStringExtra("email");
+        usernameUser = intent.getStringExtra("username");
+        passwordUser = intent.getStringExtra("password");
+        editName.setText(nameUser);
+        editEmail.setText(emailUser);
+        editUsername.setText(usernameUser);
+        editPassword.setText(passwordUser);
+    }
+}
